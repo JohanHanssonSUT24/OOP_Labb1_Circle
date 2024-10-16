6747d1145529d7436bc0caae1a7fb03bfcd96707
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-16 08:56:53 +0200

    Add project files.

---
+++ OOP_Labb1_Circle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Labb1_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
