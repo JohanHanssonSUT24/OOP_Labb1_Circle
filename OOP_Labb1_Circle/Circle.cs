714db9fe1d947e36a1438958ada17671787a9e04
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-23 07:55:43 +0200

    Finished with circle area

---
+++ OOP_Labb1_Circle/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb1_Circle
+{
+    internal class Circle
+    {
+        public float Pi { get; set; }
+        public int Radius { get; set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+            Pi = 3.141f;
+        }
+        public void GetArea()
+        {
+            Console.WriteLine($"Arean av en cirkel med radien {Radius} är: {Radius*Radius*Pi}");
+        }
+
+    }
+}
--- OOP_Labb1_Circle/Program.cs
+++ OOP_Labb1_Circle/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+            circle1.GetArea();
+            circle2.GetArea();
         }
     }
 }
